8a173bb80017e7f7b2e65579db0b7a89b3a45f85
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-03-30 04:55:22 -0400

    added grocery list to start method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,12 @@
 module PriceCalculator
     class Menu
         def start
+            milk = Item.new("Milk", "3.97")
+            milk.save_sale("2 for $5.00")
+            bread = Item.new("Bread", "2.17")
+            bread.save_sale("2 for $6.00")
+            banana = Item.new("Banana", "0.99")
+            apple = Item.new("Apple", "0.89")
             main_menu
         end
 
