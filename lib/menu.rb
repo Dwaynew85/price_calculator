a419aa7b535204d7e12b0c066796b68954330e75
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-03-31 14:20:37 -0400

    completed draft of cli menus

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,17 +4,37 @@ module PriceCalculator
             milk = Item.new("Milk", "3.97")
             milk.save_sale("2 for $5.00")
             bread = Item.new("Bread", "2.17")
-            bread.save_sale("2 for $6.00")
-            banana = Item.new("Banana", "0.99")
-            apple = Item.new("Apple", "0.89")
+            bread.save_sale("3 for $6.00")
+            Item.new("Banana", "0.99")
+            Item.new("Apple", "0.89")
             main_menu
         end
 
         def main_menu
+            puts "Please enter all the items purchased seperated by a comma"
+
             puts "Item      Unit Price       Sale Price"
             puts "-----------------------------------------"
             # iterate over a list of Items and display puts with name, price, and sales if it has a sale price
             list = gets.chomp
+            calculate_list(list)
         end
+
+        def calculate_list(list)
+            #sorts the list of items given and calculates their quanitities
+            new_list = Hash.new(0)
+            list.split(",").each { |k| new_list[k] += 1 }
+            # execute results
+        end
+
+        def results(purchased_list) # takes a lit of items and totals and displays them in the app
+            puts "Item       Quantity        Price"
+            puts "-----------------------------------------"
+            purchased_list.each { |purchase| puts "#{purchse.name}       #{purchase.quantity}               $#{purchase.total}" }
+
+            puts "Total price: $#{}" # displays total price of all items
+            puts "You saved $#{} today." # displays the difference between the total price without sales and total price with sales
+        end
+
     end
 end
